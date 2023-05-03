51f4527f9dfc1542c4e165a400ce8a5f66448ee5
maxi_malain <maxime.allain.8@gmail.com>
2023-05-03 18:25:02 +0200

    feat: add DataDescriptionCard

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./components/Home";
 export * from "./components/Root";
 export * from "./components/ErrorPage";
 export * from "./components/OpacitySlider";
+export * from "./components/DataDescriptionCard/DataDescriptionCard";
 
 export * from "./hooks/useMap";
